759b27b64cccfa6981656e66cacff3a0681d4011
Rubil <mogererubil@gmail.com>
2025-05-28 10:43:28 +0300

    new commit

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Write your code here!
+// Step 1: Remove the <main id="main">
+const main = document.getElementById('main');
+main.remove();
+
+// Step 2: Create a new <h1> element and assign to variable 'newHeader'
+const newHeader = document.createElement('h1');
+
+// Step 3: Set the ID of the new <h1> to 'victory'
+newHeader.id = 'victory';
+
+// Step 4: Set its text to "Rubil is the champion"
+newHeader.textContent = 'Rubil is the champion';
+
+// Step 5 (optional): Add the new header to the document body
+document.body.appendChild(newHeader);
